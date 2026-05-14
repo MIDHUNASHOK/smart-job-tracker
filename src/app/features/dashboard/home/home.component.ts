5e4f878aa87fd108290cfe792931cdacd77b0568
MIDHUN ASHOK <midhunashok619@gmail.com>
2026-05-14 11:59:19 +0200

    section one-status bar implementation

--- src/app/features/dashboard/home/home.component.ts
+++ src/app/features/dashboard/home/home.component.ts
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  statsCards = [
+
+    {
+      title: 'Total Applications',
+      value: 128,
+      growth: '12%',
+      type: 'positive',
+      icon: 'briefcase',
+      color: 'blue'
+    },
+  
+    {
+      title: 'Interviews',
+      value: 32,
+      growth: '8%',
+      type: 'positive',
+      icon: 'people',
+      color: 'purple'
+    },
+  
+    {
+      title: 'Offers',
+      value: 6,
+      growth: '20%',
+      type: 'positive',
+      icon: 'award',
+      color: 'green'
+    },
+  
+    {
+      title: 'Rejections',
+      value: 90,
+      growth: '5%',
+      type: 'negative',
+      icon: 'x-circle',
+      color: 'red'
+    },
+  
+    {
+      title: 'Pending',
+      value: 60,
+      growth: '8%',
+      type: 'warning',
+      icon: 'hourglass-split',
+      color: 'yellow'
+    }
+  
+  ];
 }
